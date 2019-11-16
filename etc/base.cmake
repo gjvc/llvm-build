8b622cc906264a3914bc5bd7315612b04df28d33
George Cox <gcox@nyc01node125.mf>
2019-11-16 17:32:18 -0500

    base.cmake and stage{1,2,3}.cmake files

---
+++ etc/base.cmake
@@ -0,0 +1,76 @@
+
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------------
+# .//etc/base.cmake
+# ----------------------------------------------------------------------------------------------------------------------------------------------
+#
+
+
+################################################################################################################################################
+include_guard( GLOBAL )
+
+
+####  ##############################################  ####################################################################  #####  ######  ##  #
+set(  CMAKE_ASM_COMPILER_TARGET                       ${TARGET_TRIPLE}                                                      CACHE  STRING  ""  )
+set(  CMAKE_BUILD_TYPE                                Release                                                               CACHE  STRING  ""  )
+set(  CMAKE_CXX_COMPILER_LAUNCHER                     ${CCACHE_BINARY}                                                      CACHE  STRING  ""  )
+set(  CMAKE_CXX_COMPILER_TARGET                       ${TARGET_TRIPLE}                                                      CACHE  STRING  ""  )
+set(  CMAKE_CXX_STANDARD                              11                                                                    CACHE  STRING  ""  )
+set(  CMAKE_CXX_STANDARD_REQUIRED                     YES                                                                   CACHE  STRING  ""  )
+set(  CMAKE_C_COMPILER_LAUNCHER                       ${CCACHE_BINARY}                                                      CACHE  STRING  ""  )
+set(  CMAKE_C_COMPILER_TARGET                         ${TARGET_TRIPLE}                                                      CACHE  STRING  ""  )
+set(  CMAKE_EXPORT_COMPILE_COMMANDS                   YES                                                                   CACHE  BOOL    ""  )
+set(  CMAKE_POSITION_INDEPENDENT_CODE                 YES                                                                   CACHE  BOOL    ""  )
+set(  CMAKE_VERBOSE_MAKEFILE                          YES                                                                   CACHE  BOOL    ""  )
+set(  CMAKE_EXE_LINKER_FLAGS                          "${CMAKE_EXE_LINKER_FLAGS} -Wl,-rpath,\$ORIGIN/../lib"                CACHE  STRING  ""  )
+
+####  ##############################################  ####################################################################  #####  ######  ##  #
+set(  LLVM_TARGETS_TO_BUILD                           Native                                                                CACHE  STRING  ""  )
+set(  LLVM_ENABLE_PROJECTS                            "compiler-rt;libunwind;libcxxabi;libcxx;llvm;clang;lld"               CACHE  STRING  ""  )
+set(  GCC_INSTALL_PREFIX                              /dev/null                                                             CACHE  STRING  ""  )
+
+####  ##############################################  ####################################################################  #####  ######  ##  #
+set(  LIBUNWIND_ENABLE_ASSERTIONS                     NO                                                                    CACHE  BOOL    ""  )
+set(  LIBUNWIND_ENABLE_SHARED                         YES                                                                   CACHE  BOOL    ""  )
+set(  LIBUNWIND_ENABLE_STATIC                         YES                                                                   CACHE  BOOL    ""  )
+set(  LIBUNWIND_HERMETIC_STATIC_LIBRARY               NO                                                                    CACHE  BOOL    ""  )
+set(  LIBUNWIND_INSTALL_LIBRARY                       YES                                                                   CACHE  BOOL    ""  )
+set(  LIBUNWIND_USE_COMPILER_RT                       YES                                                                   CACHE  BOOL    ""  )
+
+####  ##############################################  ####################################################################  #####  ######  ##  #
+set(  COMPILER_RT_BUILD_BUILTINS                      YES                                                                   CACHE  BOOL    ""  )
+set(  COMPILER_RT_BUILD_CRT                           YES                                                                   CACHE  BOOL    ""  )
+set(  COMPILER_RT_BUILD_LIBFUZZER                     NO                                                                    CACHE  BOOL    ""  )
+set(  COMPILER_RT_BUILD_PROFILE                       YES                                                                   CACHE  BOOL    ""  )
+set(  COMPILER_RT_BUILD_SANITIZERS                    NO                                                                    CACHE  BOOL    ""  )
+set(  COMPILER_RT_BUILD_XRAY                          YES                                                                   CACHE  BOOL    ""  )
+set(  COMPILER_RT_BUILTINS_STANDALONE_BUILD           YES                                                                   CACHE  BOOL    ""  )
+set(  COMPILER_RT_USE_BUILTINS_LIBRARY                YES                                                                   CACHE  BOOL    ""  )
+set(  COMPILER_RT_USE_LIBCXX                          YES                                                                   CACHE  BOOL    ""  )
+
+####  ##############################################  ####################################################################  #####  ######  ##  #
+set(  LIBCXXABI_ENABLE_ASSERTIONS                     NO                                                                    CACHE  BOOL    ""  )
+set(  LIBCXXABI_ENABLE_SHARED                         YES                                                                   CACHE  BOOL    ""  )
+set(  LIBCXXABI_ENABLE_STATIC                         YES                                                                   CACHE  BOOL    ""  )
+set(  LIBCXXABI_ENABLE_STATIC_UNWINDER                NO                                                                    CACHE  BOOL    ""  )
+set(  LIBCXXABI_INSTALL_LIBRARY                       YES                                                                   CACHE  BOOL    ""  )
+set(  LIBCXXABI_USE_LLVM_UNWINDER                     YES                                                                   CACHE  BOOL    ""  )
+
+####  ##############################################  ####################################################################  #####  ######  ##  #
+set(  LIBCXX_CXX_ABI_INCLUDE_PATHS                    ${SRC_PATH}/libcxxabi/include                                         CACHE  STRING  ""  )
+set(  LIBCXX_ENABLE_ASSERTIONS                        NO                                                                    CACHE  BOOL    ""  )
+set(  LIBCXX_ENABLE_SHARED                            YES                                                                   CACHE  BOOL    ""  )
+set(  LIBCXX_ENABLE_STATIC                            YES                                                                   CACHE  BOOL    ""  )
+set(  LIBCXX_HAS_GCC_S_LIB                            NO                                                                    CACHE  BOOL    ""  )
+set(  LIBCXX_INCLUDE_BENCHMARK                        NO                                                                    CACHE  BOOL    ""  )
+set(  LIBCXX_INSTALL_HEADERS                          YES                                                                   CACHE  BOOL    ""  )
+set(  LIBCXX_STATICALLY_LINK_ABI_IN_SHARED_LIBRARY    YES                                                                   CACHE  BOOL    ""  )
+set(  LIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY    YES                                                                   CACHE  BOOL    ""  )
+set(  LIBCXX_USE_COMPILER_RT                          YES                                                                   CACHE  BOOL    ""  )
+
+####  ##############################################  ####################################################################  #####  ######  ##  #
+set(  CLANG_DEFAULT_CXX_STDLIB                        libc++                                                                CACHE  STRING  ""  )
+set(  CLANG_DEFAULT_RTLIB                             compiler-rt                                                           CACHE  STRING  ""  )
+set(  CLANG_DEFAULT_UNWINDLIB                         libunwind                                                             CACHE  STRING  ""  )
+set(  LIBCLANG_BUILD_STATIC                           YES                                                                   CACHE  BOOL    ""  )
+
--- etc/stage3.cmake
+++ etc/stage3.cmake
@@ -5,54 +5,15 @@
 # ----------------------------------------------------------------------------------------------------------------------------------------------
 #
 
-
 ################################################################################################################################################
+include( ${CMAKE_CURRENT_LIST_DIR}/base.cmake )
 link_directories( ${STAGE2_TARGET_DIR}/lib/ )
 
 ####  ##############################################  ####################################################################  #####  ######  ##  #
-set(  CMAKE_VERBOSE_MAKEFILE                          YES                                                                   CACHE  BOOL    ""  )
-set(  CMAKE_BUILD_RPATH_USE_ORIGIN                    YES                                                                   CACHE  BOOL    ""  )
-set(  CMAKE_BUILD_WITH_INSTALL_RPATH                  YES                                                                   CACHE  BOOL    ""  )
-
-####  ##############################################  ####################################################################  #####  ######  ##  #
-set(  CMAKE_ASM_COMPILER_TARGET                       ${GCC_DUMPMACHINE}                                                    CACHE  STRING  ""  )
-set(  CMAKE_BUILD_TYPE                                Release                                                               CACHE  STRING  ""  )
-set(  CMAKE_CXX_COMPILER                              ${STAGE2_TARGET_DIR}/bin/clang++                                      CACHE  STRING  ""  )
-set(  CMAKE_CXX_COMPILER_LAUNCHER                     ${CCACHE_BINARY}                                                      CACHE  STRING  ""  )
-set(  CMAKE_CXX_COMPILER_TARGET                       ${GCC_DUMPMACHINE}                                                    CACHE  STRING  ""  )
-set(  CMAKE_CXX_FLAGS                                 "${CMAKE_CXX_FLAGS} -stdlib=libc++"                                   CACHE  STRING  ""  )
-set(  CMAKE_CXX_FLAGS                                 "${CMAKE_CXX_FLAGS} -pic -fPIC"                                       CACHE  STRING  ""  )
-set(  CMAKE_CXX_LINK_FLAGS                            "${CMAKE_CXX_LINK_FLAGS} -lc++"                                       CACHE  STRING  ""  )
-set(  CMAKE_CXX_LINK_FLAGS                            "${CMAKE_CXX_LINK_FLAGS} -static"                                     CACHE  STRING  ""  )
-set(  CMAKE_CXX_LINK_FLAGS                            "${CMAKE_CXX_LINK_FLAGS} -fPIE"                                       CACHE  STRING  ""  )
-set(  CMAKE_CXX_LINK_FLAGS                            "${CMAKE_CXX_LINK_FLAGS} -stdlib=libc++"                              CACHE  STRING  ""  )
-set(  CMAKE_CXX_LINK_FLAGS                            "${CMAKE_CXX_LINK_FLAGS} -rtlib=compiler-rt"                          CACHE  STRING  ""  )
-set(  CMAKE_CXX_LINK_FLAGS                            "${CMAKE_CXX_LINK_FLAGS} -Wl,-rpath,\$ORIGIN/../lib/"                 CACHE  STRING  ""  )
 set(  CMAKE_C_COMPILER                                ${STAGE2_TARGET_DIR}/bin/clang                                        CACHE  STRING  ""  )
-set(  CMAKE_C_COMPILER_LAUNCHER                       ${CCACHE_BINARY}                                                      CACHE  STRING  ""  )
-set(  CMAKE_C_COMPILER_TARGET                         ${GCC_DUMPMACHINE}                                                    CACHE  STRING  ""  )
-set(  CMAKE_EXE_LINKER_FLAGS                          "${CMAKE_EXE_LINKER_FLAGS} -lc++"                                     CACHE  STRING  ""  )
-set(  CMAKE_EXE_LINKER_FLAGS                          "${CMAKE_EXE_LINKER_FLAGS} -static"                                   CACHE  STRING  ""  )
-set(  CMAKE_EXE_LINKER_FLAGS                          "${CMAKE_EXE_LINKER_FLAGS} -pie -fPIE"                                CACHE  STRING  ""  )
-set(  CMAKE_EXE_LINKER_FLAGS                          "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -rtlib=compiler-rt"         CACHE  STRING  ""  )
-set(  CMAKE_EXE_LINKER_FLAGS                          "${CMAKE_EXE_LINKER_FLAGS} -Wl,-rpath,\$ORIGIN/../lib/"               CACHE  STRING  ""  )
+set(  CMAKE_CXX_COMPILER                              ${STAGE2_TARGET_DIR}/bin/clang++                                      CACHE  STRING  ""  )
+set(  CMAKE_EXE_LINKER_FLAGS                          "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc"                            CACHE  STRING  ""  )
+set(  CMAKE_EXE_LINKER_FLAGS                          "${CMAKE_EXE_LINKER_FLAGS} -static-libstdc++"                         CACHE  STRING  ""  )
 set(  CMAKE_INSTALL_PREFIX                            ${STAGE3_TARGET_DIR}                                                  CACHE  STRING  ""  )
 set(  CMAKE_LINKER                                    ${STAGE2_TARGET_DIR}/bin/ld.lld                                       CACHE  STRING  ""  )
-set(  CMAKE_POSITION_INDEPENDENT_CODE                 YES                                                                   CACHE  BOOL    ""  )
-set(  CMAKE_SHARED_LINKER_FLAGS                       "-stdlib=libc++ -rtlib=compiler-rt -lpthread -ldl"                    CACHE  STRING  ""  )
-set(  CMAKE_VERBOSE_MAKEFILE                          YES                                                                   CACHE  BOOL    ""  )
-
-####  ##############################################  ####################################################################  #####  ######  ##  #
-set(  BUILD_SHARED_LIBS                               NO                                                                    CACHE  BOOL    ""  )
-set(  TARGET_TRIPLE                                   ${GCC_DUMPMACHINE}                                                    CACHE  STRING  ""  )
-
-####  ##############################################  ####################################################################  #####  ######  ##  #
-set(  LLVM_TARGETS_TO_BUILD                           Native                                                                CACHE  STRING  ""  )
-set(  LLVM_BUILD_TOOLS                                NO                                                                    CACHE  BOOL    ""  )
-set(  LLVM_ENABLE_PROJECTS                            "lld;buitins;compiler-rt;libunwind;libcxxabi;libcxx;llvm;clang"       CACHE  STRING  ""  )
-
-####  ##############################################  ####################################################################  #####  ######  ##  #
-set(  CLANG_DEFAULT_CXX_STDLIB                        libc++                                                                CACHE  STRING  ""  )
-set(  CLANG_DEFAULT_RTLIB                             compiler-rt                                                           CACHE  STRING  ""  )
-set(  LIBCLANG_BUILD_STATIC                           NO                                                                    CACHE  BOOL    ""  )
 
